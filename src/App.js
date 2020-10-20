d52cab9db3a26c751f1c58d558dc97bb70449e0d
john-smilga <johnsmilga@gmail.com>
2020-10-19 20:06:34 -0700

    default text changed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 
 function App() {
-  const [markdown, setMarkdown] = useState('## awesome')
+  const [markdown, setMarkdown] = useState('# markdown preview')
 
   return (
     <main>
